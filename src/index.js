fafcff8812cebb7de852d822220ba576e2fedc56
Iryna Murzak <iramurzak@gmail.com>
2023-12-04 20:31:17 +0200

    complete task1, task2

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import { connect, close } from './connection.js';
+import { connect, close } from "./connection.js";
 
 const db = await connect();
 const usersCollection = db.collection("users");
 
 const run = async () => {
   try {
-    await getUsersExample();
+    //await getUsersExample();
     // await task1();
     // await task2();
     // await task3();
@@ -20,41 +20,61 @@ const run = async () => {
     // await task12();
 
     await close();
-  } catch(err) {
-    console.log('Error: ', err)
+  } catch (err) {
+    console.log("Error: ", err);
   }
-}
+};
 run();
 
 // #### Users
 // - Get users example
-async function getUsersExample () {
+async function getUsersExample() {
   try {
     const [allUsers, firstUser] = await Promise.all([
       usersCollection.find().toArray(),
       usersCollection.findOne(),
-    ])
+    ]);
 
-    console.log('allUsers', allUsers);
-    console.log('firstUser', firstUser);
+    console.log("allUsers", allUsers);
+    console.log("firstUser", firstUser);
   } catch (err) {
-    console.error('getUsersExample', err);
+    console.error("getUsersExample", err);
   }
 }
 
 // - Get all users, sort them by age (ascending), and return only 5 records with firstName, lastName, and age fields.
-async function task1 () {
+async function task1() {
   try {
+    const [sortedUsers] = await Promise.all([
+      usersCollection
+        .find({})
+        .project({ firstName: 1, lastName: 1, age: 1, _id: 0 })
+        .sort({ age: 1 })
+        .limit(5)
+        .toArray(),
+    ]);
 
+    console.log("sortedUsers", sortedUsers);
   } catch (err) {
     console.error('task1', err)
   }
 }
 
 // - Add new field 'skills: []" for all users where age >= 25 && age < 30 or tags includes 'Engineering'
-async function task2 () {
+async function task2() {
   try {
-    
+    const result = await usersCollection.updateMany(
+      {
+        $or: [{ age: { $gte: 25, $lt: 30 } }, { tags: "Engineering" }],
+      },
+      {
+        $set: { skills: [] },
+      }
+    );
+
+    console.log(
+      `${result.modifiedCount} users updated with the 'skills' field.`
+    );
   } catch (err) {
     console.error('task2', err)
   }
@@ -72,39 +92,36 @@ async function task3() {
 
 // - REPLACE the first document where the 'email' field starts with 'john' and the 'address state' is equal to 'CA'
 //   Set firstName: "Jason", lastName: "Wood", tags: ['a', 'b', 'c'], department: 'Support'
-async function task4 () {
+async function task4() {
   try {
 
   } catch (err) {
-    console.log('task4', err);
+    console.log("task4", err);
   }
 }
 
 // - Pull tag 'c' from the first document where firstName: "Jason", lastName: "Wood"
-async function task5 () {
+async function task5() {
   try {
-
   } catch (err) {
-    console.log('task5', err);
+    console.log("task5", err);
   }
 }
 
 // - Push tag 'b' to the first document where firstName: "Jason", lastName: "Wood"
 //   ONLY if the 'b' value does not exist in the 'tags'
-async function task6 () {
+async function task6() {
   try {
-
   } catch (err) {
-    console.log('task6', err);
+    console.log("task6", err);
   }
 }
 
 // - Delete all users by department (Support)
-async function task7 () {
+async function task7() {
   try {
-
   } catch (err) {
-    console.log('task7', err);
+    console.log("task7", err);
   }
 }
 
@@ -114,47 +131,42 @@ async function task7 () {
 //   Find articles with type a, and update tag list with next value ['tag1-a', 'tag2-a', 'tag3']
 //   Add tags ['tag2', 'tag3', 'super'] to articles except articles with type 'a'
 //   Pull ['tag2', 'tag1-a'] from all articles
-async function task8 () {
+async function task8() {
   try {
-    
   } catch (err) {
-    console.error('task8', err);
+    console.error("task8", err);
   }
 }
 
 // - Find all articles that contains tags 'super' or 'tag2-a'
-async function task9 () {
+async function task9() {
   try {
-
   } catch (err) {
-    console.log('task9', err);
+    console.log("task9", err);
   }
 }
 
 // #### Students Statistic (Aggregations)
 // - Find the student who have the worst score for homework, the result should be [ { name: <name>, worst_homework_score: <score> } ]
-async function task10 () {
+async function task10() {
   try {
-
   } catch (err) {
-    console.log('task10', err);
-  } 
+    console.log("task10", err);
+  }
 }
 
 // - Calculate the average score for homework for all students, the result should be [ { avg_score: <number> } ]
-async function task11 () {
+async function task11() {
   try {
-
   } catch (err) {
-    console.log('task11', err);
-  } 
+    console.log("task11", err);
+  }
 }
 
 // - Calculate the average score by all types (homework, exam, quiz) for each student, sort from the largest to the smallest value
-async function task12 () {
+async function task12() {
   try {
-
   } catch (err) {
-    console.log('task12', err);
-  } 
+    console.log("task12", err);
+  }
 }
